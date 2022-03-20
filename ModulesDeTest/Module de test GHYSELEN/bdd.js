ed2aca612c2f14d7779dd6d042a727597c1ff734
Lucas Ghyselen <70692658+LucasGLaPro@users.noreply.github.com>
2022-03-20 16:15:48 +0100

    Ajout du module de test de Lucas Ghyselen

---
+++ ModulesDeTest/Module de test GHYSELEN/bdd.js
@@ -0,0 +1,51 @@
+﻿const mysql = require('mysql');
+
+
+
+
+class bdd {
+    constructor(login, mdp, ip){
+        /*this.login = _login;
+        this.mdp = _mdp;
+        this.ip = _ip;*/
+
+
+
+        this.db = mysql.createConnection({
+
+            host: "mysql-lucaslapro.alwaysdata.net",
+         
+            user: login,
+         
+            password: mdp,
+
+            database : ip
+         
+          })
+          this.db.connect(function(err) {
+            if (err) throw err;
+            console.log("Connecté à la base de données MySQL!");
+          })
+
+          /*db.query("SELECT * FROM users", function (err, result) {
+            if (err) throw err;
+            console.log(result);
+          })*/
+          ;
+
+    }
+
+    GetAllUser() {
+
+      return new Promise( ( resolve, reject ) => {
+        this.db.query( "SELECT * FROM users", function ( err, result ) {
+          if ( err ) reject( err )
+          resolve( result );
+        })
+      })
+    
+    }
+}
+
+module.exports = bdd
+//new bdd('mysql-lucaslapro.alwaysdata.net', '229070_lucas', 'lucasglapro');
