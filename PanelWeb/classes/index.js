3e4b632aa340493761e6cf251d54b432890d7c47
LucasGLaPro <70692658+LucasGLaPro@users.noreply.github.com>
2022-04-06 10:40:50 +0200

    avec l'index

--- PanelWeb/classes/index.js
+++ PanelWeb/classes/index.js
@@ -18,6 +18,7 @@ const vm = require("./VM.js");
     //await test.DeleteUser("7")
     //await test.GetUserbyId("1") 
     //await test.EditUser("4", "phillipouille", "greg", "mionculcestbonla","" ) 
-    console.log(await test.FindSession("GHDyLRVGn8_NuAmhftMA3uvwAeMO3i0Dd"))
+    //console.log(await test.FindSession("GHDyLRVGn8_NuAmhftMA3uvwAeMO3i0Dd"))
     //await test.CreateSession("33","FKJDGHDFKGHDFLG")
+    console.log( await test.ConnectUser("chibre", "test"))
 })( );
