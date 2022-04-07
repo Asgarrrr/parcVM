76185b478a9442f2197ca0bb69dd1beaefe7eca2
LucasGLaPro <70692658+LucasGLaPro@users.noreply.github.com>
2022-04-07 10:15:39 +0200

    SHA512

--- PanelWeb/classes/User.js
+++ PanelWeb/classes/User.js
@@ -23,7 +23,7 @@ class User {
     CreateUser(Nom, Prenom, Email, MDP, admin) {
 
       return new Promise( ( resolve, reject ) => {
-        this.bdd.query( "INSERT INTO `users` VALUES ( NULL, '"+Nom+"', '"+Prenom+"', '"+Email+"', MD5('"+MDP+"'), "+(admin= 0)+")", function ( err, result ) {
+        this.bdd.query( "INSERT INTO `users` VALUES ( NULL, '"+Nom+"', '"+Prenom+"', '"+Email+"', SHA2('"+MDP+"',512), "+(admin= 0)+")", function ( err, result ) {
           if ( err ) reject( err )
           resolve( result );
         })
@@ -91,7 +91,7 @@ class User {
       
       return new Promise( ( resolve, reject ) => {
 
-        this.bdd.query( "SELECT Email, IdUser, Nom, Prenom, admin FROM `users` WHERE Email = '"+email+"' AND  MD5('"+mdp+"') LIMIT 1;", function ( err, result ) {
+        this.bdd.query( "SELECT Email, IdUser, Nom, Prenom, admin FROM `users` WHERE Email = '"+email+"' AND  SHA2('"+mdp+"',512) LIMIT 1;", function ( err, result ) {
           if ( err ) reject( err )
           resolve( result )
         })
