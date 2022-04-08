93180c871b7611a55127fee2de6be6fedc5ef97a
Asgarrrr <caruellej@hotmail.fr>
2022-04-08 13:46:13 +0200

    Logout + avoiding dual connexion + Login cookies

--- PanelWeb/routes/index.js
+++ PanelWeb/routes/index.js
@@ -12,42 +12,106 @@ const User = require( "./../classes/User" );
 // —— Route index
 router.get( "/", deserializeSession, ( req, res, next ) => {
 
-    res.render( "index", {
-        title: "Tuduuuuuuu",
-    } );
+    if ( req.session.user.admin ) {
+        res.render( "admin", {
+            user: req.session.user
+        });
+    } else {
+        res.render( "random" );
+    }
 
 });
 
 // —— login index
-router.get( "/login", ( req, res, next ) => {
+router.get( "/login", async ( req, res, next ) => {
+
+    const { iLoveYou } = req.cookies;
+
+    if ( iLoveYou ) {
+
+        const sessionID = cookieParser
+            .signedCookie( iLoveYou, process.env.SESSION_SECRET )
+            .toString();
+
+        if ( sessionID ) {
+
+            const user = await new User( req.app.get( "BDD" ).getBDD( ) ).FindSession( sessionID );
+
+            if ( user.length )
+                return res.redirect( "/" );
+
+        }
+
+    }
 
     res.render( "login" );
 
 });
 
-router.post( "/login", ( req, res, next ) => {
+router.post( "/login", async ( req, res, next ) => {
+
 
    try {
 
         const { email, password } = req.body;
 
         if ( !( email && password ) )
-            res.status( 418 ).send( "I'm a teapot" );
+            return res.status( 418 ).send( "Email and password are required" );
 
         if ( !email )
-            res.status( 418  ).send( "Email required" );
+            return res.status( 418 ).send( "Email required" );
 
         if ( !password )
-            res.status( 418 ).send( "Password required" );
+            return res.status( 418 ).send( "Password required" );
 
-        console.log( email, password )
+        const user = await new User( req.app.get( "BDD" ).getBDD( ) );
+        const resp = await user.ConnectUser( email, password );
 
-        const user = await new User( req.app.get( "BDD" ).getDB( ) );
+        if ( !resp || !resp.length )
+            return res.status( 418 ).send( "No users found with this login/password combination" );
 
-        .ConnectUser( email, password );
+        user.CreateSession( resp[ 0 ].IdUser, req.sessionID );
+        req.session.user = resp[ 0 ];
+
+        res.redirect( "/")
 
     } catch ( error ) {
         console.error( error )
+    }
+
+});
+
+router.get( "/logout", async ( req, res, next ) => {
+
+    try {
+
+        const { iLoveYou } = req.cookies;
+
+         if ( iLoveYou ) {
+
+            const sessionID = cookieParser
+                .signedCookie( iLoveYou, process.env.SESSION_SECRET )
+                .toString();
+
+            if ( sessionID ) {
+
+                const user      = await new User( req.app.get( "BDD" ).getBDD( ) )
+                const sessions  = await user.FindSession( sessionID );
+
+                if ( sessions.length )
+                    user.DeleteSession( sessionID );
+
+            }
+
+        }
+
+        res.redirect( "./login" );
+
+    } catch ( error ) {
+
+        res.status( 418 ).send( error );
+        console.error( error );
+
     }
 
 });
