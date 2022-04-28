d18214606b70ac6db858ad433cb47cb621ca81e8
Asgarrrr <caruellej@hotmail.fr>
2022-04-28 08:06:51 +0200

    Login verification middleware

--- PanelWeb/Utils/sessions.js
+++ PanelWeb/Utils/sessions.js
@@ -7,7 +7,7 @@ async function deserializeSession( req, res, next ) {
     const { iLoveYou } = req.cookies;
 
     if ( !iLoveYou )
-        return next();
+        return res.redirect( "/login" );
 
     const sessionID = cookieParser
         .signedCookie( iLoveYou, process.env.SESSION_SECRET )
@@ -15,12 +15,12 @@ async function deserializeSession( req, res, next ) {
 
     if ( sessionID ) {
 
-        const user = await new User( req.app.get( "BDD" ).getDB( ) ).FindSession( sessionID );
+        const user = await new User( req.app.get( "BDD" ).getBDD( ) ).FindSession( sessionID );
 
         if ( !user.length )
             return res.redirect( "/login" );
 
-        req.session.user = user;
+        req.session.user = user[ 0 ];
 
     }
 
