8cdf4a2f902add2f0d0ffe31a47913592c0bba7d
Asgarrrr <caruellej@hotmail.fr>
2022-05-14 14:00:45 +0200

    subsection router

--- PanelWeb/routes/index.js
+++ PanelWeb/routes/index.js
@@ -1,19 +1,15 @@
-// ██████ Dependencies ████████████████████████████████████████████████████████
 
-const cookieParser  = require( "cookie-parser"  )
-    , express       = require( "express"        );
+const { Router } = require( "express" );
+const router = Router( );
+const auth   = require( "../middlewares/auth" );
 
-const { deserializeSession } = require( "../Utils/sessions" );
-
-const router        = express.Router( );
-
-const User = require( "./../classes/User" );
+const User = require( "../classes/User" );
 
 // —— Route index
-router.get( "/", deserializeSession, ( req, res, next ) => {
+router.get( "/", auth, ( req, res, next ) => {
 
     if ( req.session.user.admin ) {
-        res.render( "admin", {
+        res.render( "./admin/index", {
             user: req.session.user
         });
     } else {
@@ -22,27 +18,44 @@ router.get( "/", deserializeSession, ( req, res, next ) => {
 
 });
 
+router.get( "/vm", auth, ( req, res, next ) => {
+
+    if ( req.session.user.admin ) {
+        res.render( "./admin/vm", {
+            user: req.session.user
+        });
+    } else {
+        res.render( "random" );
+    }
+
+} );
+
+router.get( "/dashboard", auth, ( req, res, next ) => {
+
+    if ( req.session.user.admin ) {
+        res.render( "./admin/dashboard", {
+            user: req.session.user
+        });
+    } else {
+        res.render( "random" );
+    }
+
+} );
+
+router.get( "/project", auth, ( req, res, next ) => {
+
+    if ( req.session.user.admin ) {
+        res.render( "./admin/project", {
+            user: req.session.user
+        });
+    } else {
+        res.render( "random" );
+    }
+
+} );
+
 // —— login index
 router.get( "/login", async ( req, res, next ) => {
-
-    const { iLoveYou } = req.cookies;
-
-    if ( iLoveYou ) {
-
-        const sessionID = cookieParser
-            .signedCookie( iLoveYou, process.env.SESSION_SECRET )
-            .toString();
-
-        if ( sessionID ) {
-
-            const user = await new User( req.app.get( "BDD" ).getBDD( ) ).FindSession( sessionID );
-
-            if ( user.length )
-                return res.redirect( "/" );
-
-        }
-
-    }
 
     res.render( "login" );
 
@@ -64,11 +77,14 @@ router.post( "/login", async ( req, res, next ) => {
         if ( !password )
             return res.status( 418 ).send( "Password required" );
 
-        const user = await new User( req.app.get( "BDD" ).getBDD( ) );
+        const user = await new User( req.app.get( "BDD" ) );
         const resp = await user.ConnectUser( email, password );
 
         if ( !resp || !resp.length )
             return res.status( 418 ).send( "No users found with this login/password combination" );
+
+
+        console.log( resp[ 0 ].IdUser )
 
         user.CreateSession( resp[ 0 ].IdUser, req.sessionID );
         req.session.user = resp[ 0 ];
@@ -81,30 +97,13 @@ router.post( "/login", async ( req, res, next ) => {
 
 });
 
-router.get( "/logout", async ( req, res, next ) => {
+router.get( "/logout", auth, async ( req, res, next ) => {
 
     try {
 
-        const { iLoveYou } = req.cookies;
-
-         if ( iLoveYou ) {
-
-            const sessionID = cookieParser
-                .signedCookie( iLoveYou, process.env.SESSION_SECRET )
-                .toString();
-
-            if ( sessionID ) {
-
-                const user      = await new User( req.app.get( "BDD" ).getBDD( ) )
-                const sessions  = await user.FindSession( sessionID );
-
-                if ( sessions.length )
-                    user.DeleteSession( sessionID );
-
-            }
-
-        }
-
+        const user = await new User( req.app.get( "BDD" ).getBDD( ) );
+        await user.DeleteSession( req.session.user.sessionID );
+        req.session.destroy( );
         res.redirect( "./login" );
 
     } catch ( error ) {
