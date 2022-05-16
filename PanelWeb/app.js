dea4b8179f20c40a4d7cae0a83c4af37f21553f6
Asgarrrr <caruellej@hotmail.fr>
2022-05-16 22:44:59 +0200

    Change error logic

--- PanelWeb/app.js
+++ PanelWeb/app.js
@@ -244,21 +244,10 @@ function normalizePort( val ) {
  * @throws  { object }
  */
 function onError( error ) {
+
     if ( error.syscall !== "listen" )
         throw error;
 
-    const bind = typeof port === "string" ? `Pipe ${ port }` : `Port ${ port }`;
+    console.log( "Error: " + error );
 
-    // —— Handle specific listen errors with super friendly messages :)
-    switch ( error.code ) {
-        case "EACCES":
-            console.error( `${ bind } requires elevated privileges` );
-            process.exit( 1 );
-        case "EADDRINUSE":
-            console.error( `${ bind } is already in use` );
-            process.exit( 1 );
-        default:
-            // —— In all other cases, throw the error, because I don't know what to do.
-            throw error;
-    }
 }
