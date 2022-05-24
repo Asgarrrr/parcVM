ffbd9c8459a33d05b23efd3fdcb39b732b92f104
Asgarrrr <caruellej@hotmail.fr>
2022-05-24 09:42:47 +0200

    update

--- PanelWeb/routes/API.js
+++ PanelWeb/routes/API.js
@@ -1,11 +1,30 @@
 // ██████ Dependencies ████████████████████████████████████████████████████████
 
-const express       = require( "express"        );
+const express = require( "express"        );
 
 const auth = require( "../middlewares/auth" );
 
 const router = express.Router( );
 
+const Temp = require( "../classes/temperature" );
+
+
+// —— Save temperature on the database
+router.post( "/temp", async ( req, res, next ) => {
+
+    try {
+
+        // —— Save temperature on the database
+        new Temp( req.app.get( "BDD" ) ).insertTemp( req.query.deg );
+        return res.status( 200 ).send( "OK" );
+
+    } catch ( error ) {
+        console.log( error );
+        res.sendStatus( 500 );
+
+    }
+
+});
 
 // —— Route index
 router.get( "/getVMs", auth, async ( req, res, next ) => {
