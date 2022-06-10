5770358300ae95169cbb55fbd39fa720fec9635d
Asgarrrr <caruellej@hotmail.fr>
2022-06-10 08:26:46 +0200

    goodbye !

--- PanelWeb/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require( "express" )
-    , router  = express.Router();
-
-// —— Route index
-router.get( "/", ( req, res, next ) => {
-    res.render( "users", { } );
-});
-
-module.exports = router;
